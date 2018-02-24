b6255da19a78dd8cbe19b976020bc9ba9267b38c
josue rojas <josuerojas.rojas@gmail.com>
2018-02-24 17:39:04 -0500

    basic menus function added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import InputTable from './InputTable'
-import * as Loaders from './LoaderMakers';
+import InputTable from './Components/View/InputTable'
+import SideMenu from './Components/View/SideMenu'
+import * as Loaders from './Components/View/LoaderMakers';
 
 // BUGS BUGS BUGS (and features):
-// does keypress navigation does not work when resize (need to check resize and update)
 // booleans should be 0 or 1 in loaders to make it easier to change in input
-// should have navigation button so mobile can use the auto scrolling
-// add smooth auto scroll
 
 // a FullView is a div that represents a full screen page with everything center.....simple..... (a slideshow sort of)
 class FullView extends Component{
@@ -66,91 +64,141 @@ class InnerView extends Component{
   }
 }
 
-class App extends Component {
+// class App extends Component {
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       positions: [],
+//       viewSize: 0,
+//       windowHeight: 0,
+//     }
+//     this.handleResize = this.handleResize.bind(this);
+//     this.findNext = this.findNext.bind(this);
+//     this.handleKeys = this.handleKeys.bind(this);
+//   }
+//   componentDidMount(){
+//     this.handleResize();
+//     window.addEventListener('resize', this.handleResize);
+//   }
+//   componentWillUnmount(){
+//     window.removeEventListener('resize', this.handleResize);
+//   }
+//   handleResize(){
+//     const views = document.getElementsByClassName('Full-View');
+//     const viewsPosition = [];
+//     const viewSize = window.innerHeight;
+//     let viewPos = 0;
+//     for(let i = 0; i < views.length; i++){
+//       viewsPosition.push(viewPos);
+//       viewPos+=viewSize;
+//     }
+//     this.setState({
+//       positions: viewsPosition,
+//       viewSize: viewSize,
+//     })
+//   }
+//   findNext(currPos, forward){
+//     // cant divide by zero
+//     if(currPos === 0) return forward ? this.state.positions[1] : this.state.positions[-1] ;
+//     return this.state.positions[Math.floor(currPos / this.state.viewSize) + (forward ? 1 : -1)];
+//   }
+//   handleKeys(e){
+//     let nextPos = undefined;
+//     let currPos = window.pageYOffset;
+//     switch(e.keyCode){
+//       case 37:
+//         // left
+//         nextPos = this.findNext(currPos, false)
+//         break;
+//       case 38:
+//         // up
+//         nextPos = this.findNext(currPos, false)
+//         break;
+//       case 39:
+//         // right
+//         nextPos = this.findNext(currPos, true)
+//         break;
+//       case 40:
+//         // down
+//         nextPos = this.findNext(currPos, true)
+//         break;
+//       default:
+//         break;
+//     }
+//     if(nextPos !== undefined) window.scroll(0, nextPos);
+//   }
+//   // make views automatically just add them in Loaders.Makers array
+//   makeViews(height){
+//     const Views = [];
+//     for(let i = 0; i < Loaders.Makers.length; i++){
+//       Views.push(
+//         <FullView
+//           key={i+'FullView'}
+//           stuff={(<InnerView maker={Loaders.Makers[i]}/>)}
+//           bgColor={'rgb(225,225,225)'}
+//           height={height}/>
+//       )
+//     }
+//     return Views;
+//   }
+//   render() {
+//     const Views = this.makeViews(this.state.viewSize);
+//     return (
+//       <div onKeyDown={this.handleKeys} tabIndex="0">
+//         <SideMenu
+//           backgroundColor='rgba(50,50,200,.5)'
+//           positions='left'
+//           menuType='dots'>
+//           Hello
+//         </SideMenu>
+//         <SideMenu
+//           backgroundColor='rgba(200,50,200,.5)'
+//           positions='right'
+//           menuType='Hamburger'>
+//           bye
+//         </SideMenu>
+//       </div>
+//     );
+//   }
+// }
+
+export default class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      positions: [],
-      viewSize: 0,
-      windowHeight: 0,
+      leftMenu: false,
+      rightMenu: false,
     }
-    this.handleResize = this.handleResize.bind(this);
-    this.findNext = this.findNext.bind(this);
-    this.handleKeys = this.handleKeys.bind(this);
+    this.handleMenuClick = this.handleMenuClick.bind(this);
   }
-  componentDidMount(){
-    this.handleResize();
-    window.addEventListener('resize', this.handleResize);
-  }
-  componentWillUnmount(){
-    window.removeEventListener('resize', this.handleResize);
-  }
-  handleResize(){
-    const views = document.getElementsByClassName('Full-View');
-    const viewsPosition = [];
-    const viewSize = window.innerHeight;
-    let viewPos = 0;
-    for(let i = 0; i < views.length; i++){
-      viewsPosition.push(viewPos);
-      viewPos+=viewSize;
-    }
+  handleMenuClick(menu){
     this.setState({
-      positions: viewsPosition,
-      viewSize: viewSize,
+      leftMenu: menu === 'left' ? !this.state.leftMenu : false,
+      rightMenu: menu === 'right' ? !this.state.rightMenu : false,
     })
   }
-  findNext(currPos, forward){
-    // cant divide by zero
-    if(currPos === 0) return forward ? this.state.positions[1] : this.state.positions[-1] ;
-    return this.state.positions[Math.floor(currPos / this.state.viewSize) + (forward ? 1 : -1)];
-  }
-  handleKeys(e){
-    let nextPos = undefined;
-    let currPos = window.pageYOffset;
-    switch(e.keyCode){
-      case 37:
-        // left
-        nextPos = this.findNext(currPos, false)
-        break;
-      case 38:
-        // up
-        nextPos = this.findNext(currPos, false)
-        break;
-      case 39:
-        // right
-        nextPos = this.findNext(currPos, true)
-        break;
-      case 40:
-        // down
-        nextPos = this.findNext(currPos, true)
-        break;
-      default:
-        break;
-    }
-    if(nextPos !== undefined) window.scroll(0, nextPos);
-  }
-  // make views automatically just add them in Loaders.Makers array
-  makeViews(height){
-    const Views = [];
-    for(let i = 0; i < Loaders.Makers.length; i++){
-      Views.push(
-        <FullView
-          key={i+'FullView'}
-          stuff={(<InnerView maker={Loaders.Makers[i]}/>)}
-          bgColor={'rgb(225,225,225)'}
-          height={height}/>
-      )
-    }
-    return Views;
-  }
   render() {
-    const Views = this.makeViews(this.state.viewSize);
     return (
       <div onKeyDown={this.handleKeys} tabIndex="0">
-        {Views}
+        <SideMenu
+          backgroundColor='rgba(50,50,200,.5)'
+          positions='left'
+          active={this.state.leftMenu}
+          handleMenuClick={this.handleMenuClick}
+          menuType='Dots'>
+          Hello
+        </SideMenu>
+        <SideMenu
+          backgroundColor='rgba(200,50,200,.5)'
+          positions='right'
+          active={this.state.rightMenu}
+          handleMenuClick={this.handleMenuClick}
+          menuType='Hamburger'>
+          bye
+        </SideMenu>
       </div>
     );
   }
 }
 
-export default App;
+// export default App;
